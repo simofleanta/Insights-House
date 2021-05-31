895aeaf1f8e592cebcdda4b1cfc83c400473cdd6
simofleanta <simo.fleanta@gmail.com>
2021-05-31 16:12:27 +0200

    Add files via upload

---
+++ Toggle menue.js
@@ -0,0 +1,11 @@
+<script>
+var navLinks = document.getElementById("navLinks");
+
+function showMeneu(){
+     navLinks.style.right ="0"}
+
+function hideMeneu(){
+    navLinks.style.right ="-350px"
+}
+ 
+</script>
